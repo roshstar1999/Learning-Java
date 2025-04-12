08a63b32d21bfe4cd905421cccad04f5d34381de
N V Roshni <roshstar1999@gmail.com>
2025-04-12 06:06:50 +0530

    Create 3_math.java

---
+++ 3_math.java
@@ -0,0 +1,24 @@
+public class Main{
+    public static void main(String[] args ){
+        
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        
+        double result;
+        double res=Math.pow(2,3);
+            System.out.println("Math.pow(2,3)" + res);
+        double res2=Math.abs(-2.2);
+        
+        System.out.println(" Math.abs(-2.2)" + res2);
+        // max,min,floor,ceil,abs.sqrt,etc;
+        int a=4;
+        int b=3;
+        
+        double hyp=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.println(hyp);
+        
+        
+        
+        
+    }
+}
